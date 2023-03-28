a86d06d14b8580db1a9ece7fdf0425a6698ee00b
syariil <syahrilditoro@gmail.com>
2023-03-28 10:27:59 +0700

    view tampil tapi tombol create dan yang lain tidak berfungsi(ajax tidak berfungsi)

---
+++ backend/modules/referensi/views/ref-kelamin/_form.php
@@ -0,0 +1,37 @@
+<?php
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\referensi\RefKelamin */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="ref-kelamin-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'JENIS_KELAMIN')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'CREATE_BY')->textInput() ?>
+
+    <?= $form->field($model, 'CREATE_DATE')->textInput() ?>
+
+    <?= $form->field($model, 'CREATE_IP')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'UPDATE_BY')->textInput() ?>
+
+    <?= $form->field($model, 'UPDATE_DATE')->textInput() ?>
+
+    <?= $form->field($model, 'UPDATE_IP')->textInput(['maxlength' => true]) ?>
+
+  
+	<?php if (!Yii::$app->request->isAjax){ ?>
+	  	<div class="form-group">
+	        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+	    </div>
+	<?php } ?>
+
+    <?php ActiveForm::end(); ?>
+    
+</div>
---
+++ backend/modules/referensi/views/ref-kelamin/index.php
@@ -0,0 +1,66 @@
+<?php
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\bootstrap4\Modal;
+use kartik\grid\GridView;
+use johnitvn\ajaxcrud\CrudAsset;
+use johnitvn\ajaxcrud\BulkButtonWidget;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\modules\referensi\models\RefKelaminSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Ref Kelamins';
+$this->params['breadcrumbs'][] = $this->title;
+
+CrudAsset::register($this);
+
+?>
+<div class="ref-kelamin-index">
+    <div id="ajaxCrudDatatable">
+        <?=GridView::widget([
+            'id'=>'crud-datatable',
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'pjax'=>true,
+            'columns' => require(__DIR__.'/_columns.php'),
+            'toolbar'=> [
+                ['content'=>
+                    Html::a('<i class="fas fa-plus"></i>', ['create'],
+                    ['role'=>'modal-remote','title'=> 'Create new Ref Kelamins','class'=>'btn btn-secondary']).
+                    Html::a('<i class="fas fa-redo"></i>', [''],
+                    ['data-pjax'=>1, 'class'=>'btn btn-secondary', 'title'=>'Reset Grid']).
+                    '{toggleData}'.
+                    '{export}'
+                ],
+            ],
+            'striped' => true,
+            'condensed' => true,
+            'responsive' => true,
+            'panel' => [
+                'type' => 'primary',
+                'heading' => '<i class="fas fa-list-alt"></i> Ref Kelamins listing',
+                'before'=>'<em>* Resize table columns just like a spreadsheet by dragging the column edges.</em>',
+                'after'=>BulkButtonWidget::widget([
+                            'buttons'=>Html::a('<i class="fas fa-trash"></i>&nbsp; Delete All',
+                                ["bulkdelete"] ,
+                                [
+                                    "class"=>"btn btn-danger btn-xs",
+                                    'role'=>'modal-remote-bulk',
+                                    'data-confirm'=>false, 'data-method'=>false,// for overide yii data api
+                                    'data-request-method'=>'post',
+                                    'data-confirm-title'=>'Are you sure?',
+                                    'data-confirm-message'=>'Are you sure want to delete this item'
+                                ]),
+                        ]).
+                        '<div class="clearfix"></div>',
+            ]
+        ])?>
+    </div>
+</div>
+<?php Modal::begin([
+    "id"=>"ajaxCrudModal",
+    "title" => '<h4 class="modal-title">Modal title</h4>',
+    "footer"=>"",// always need it for jquery plugin
+])?>
+<?php Modal::end(); ?>
