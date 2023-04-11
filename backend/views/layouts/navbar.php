64b25c7240ae6e3b5d6834c53ebd34e7e401c252
syariil <syahrilditoro@gmail.com>
2023-04-11 14:30:32 +0700

    update views login dan menambahkan access santhika29

--- backend/views/layouts/navbar.php
+++ backend/views/layouts/navbar.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            <a class="nav-link " data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
             <a href="<?=\yii\helpers\Url::home()?>" class="nav-link">Home</a>
